abec750b470bd867ec3bc1bef89f90c6dbe8698d
KingArcherTheThird <KingArcherTheThird@users.noreply.github.com>
2016-12-08 16:48:22 +0100

    Create setup.java

---
+++ setup.java
@@ -0,0 +1,14 @@
+/*
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+*/
+
+public class setup
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello f*cking world!!");
+  }
+}
